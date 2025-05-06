b8ff8931fe7d867dbb027ef7e0cd9e0faa1286ea
Gummientchen <steff.freak@gmail.com>
2025-05-06 21:07:42 +0200

    refactored search_results.html

---
+++ display_config.py
@@ -0,0 +1,226 @@
+# --- Display Labels for Known Columns ---
+DISPLAY_LABELS = {
+    "cars_tg_code": "TG-Code (Typen&shy;genehmigungs&shy;nummer)",
+    "cars_col_01_fahrzeugart_value": "Fahrzeugart",
+    "cars_col_02_fahrzeugsubart_code_value": "Fahrzeugsubart Code",
+    "cars_col_02_fahrzeugsubart_value": "Fahrzeugsubart",
+    "cars_col_03_fahrzeugklasse_value": "Fahrzeugklasse",
+    "cars_col_04_marke_value": "Marke",
+    "cars_col_04_typ_value": "Typ",
+    "cars_col_06_vorziffer_value": "Vorziffer",
+    "cars_col_07_karosserieform_code_value": "Karosserieform Code",
+    "cars_col_07_karosserieform_value": "Karosserieform",
+    "cars_col_09_eu_gesamtgenehmigung_value": "EU Gesamtgenehmigung",
+    "cars_col_10_hersteller_value": "Hersteller (Code 10)",
+    "cars_col_10_efko_code_value": "EFKO Code",
+    "cars_col_11_herstellerplakette_value": "Herstellerplakette",
+    "cars_col_12_fahrgestellnummer_struktur_value": "Fahrgestellnummer Struktur", 
+    "cars_col_12_fahrgestellnummer_value": "Fahrgestellnummer", 
+    "cars_col_14_achsen_raeder_value": "Achsen/Räder",
+    "cars_col_15_federung_value": "Federung",
+    "cars_col_16_lenkung_value": "Lenkung",
+    "cars_col_17_achsantrieb_value": "Achsantrieb",
+    "cars_col_18_getriebe_1_value": "Getriebe 1 Art",
+    "cars_col_18_zuordnung_1_value": "Getriebe 1 Zuordnung",
+    "cars_col_18_getriebe_2_value": "Getriebe 2 Art",
+    "cars_col_18_zuordnung_2_value": "Getriebe 2 Zuordnung",
+    "cars_col_18_getriebe_3_value": "Getriebe 3 Art",
+    "cars_col_18_zuordnung_3_value": "Getriebe 3 Zuordnung",
+    "cars_col_18_getriebe_4_value": "Getriebe 4 Art",
+    "cars_col_18_zuordnung_4_value": "Getriebe 4 Zuordnung",
+    "cars_col_20_betriebsbremse_z1_value": "Betriebsbremse Z1",
+    "cars_col_20_betriebsbremse_z2_value": "Betriebsbremse Z2",
+    "cars_col_20_betriebsbremse_z3_value": "Betriebsbremse Z3",
+    "cars_col_21_hilfsbremse_z1_value": "Hilfsbremse Z1",
+    "cars_col_21_hilfsbremse_z2_value": "Hilfsbremse Z2",
+    "cars_col_22_feststellbremse_value": "Feststellbremse",
+    "cars_col_25_motor_marke_value": "Motor Marke",
+    "cars_col_25_motor_typ_value": "Motor Typ",
+    "cars_col_26_bauart_value": "Motor Bauart",
+    "cars_col_26_bauart_treibstoff_value": "Treibstoff (Motor)",
+    "cars_col_30_abgasreinigung_value": "Abgasreinigung",
+    "cars_col_31_schalldaempfer_1_art_value": "Schalldämpfer 1 Art",
+    "cars_col_31_schalldaempfer_1_bez_value": "Schalldämpfer 1 Bez.",
+    "cars_col_32_schalldaempfer_2_art_value": "Schalldämpfer 2 Art",
+    "cars_col_32_schalldaempfer_2_bez_value": "Schalldämpfer 2 Bez.",
+    "cars_col_33_schalldaempfer_3_art_value": "Schalldämpfer 3 Art",
+    "cars_col_33_schalldaempfer_3_bez_value": "Schalldämpfer 3 Bez.",
+    "cars_col_34_motorkennzeichen_art_value": "Motorkennzeichen Art", 
+    "cars_col_34_motorkennzeichen_value": "Motorkennzeichen", 
+    "cars_col_34_motorkennzeichen_anbringungsort_value": "Motorkennzeichen Anbringungsort",
+    "cars_col_35_geraeuschdaempfung_z1_value": "Geräuschdämpfung Z1",
+    "cars_col_35_geraeuschdaempfung_z2_value": "Geräuschdämpfung Z2",
+    "cars_col_35_geraeuschdaempfung_z3_value": "Geräuschdämpfung Z3",
+    "cars_col_35_geraeuschdaempfung_z4_value": "Geräuschdämpfung Z4",
+    "cars_col_37_anzahl_plaetze_vorne": "Anzahl Plätze Vorne",
+    "cars_col_37_anzahl_plaetze_total": "Anzahl Plätze Total",
+    "cars_col_38_anzahl_tueren_value": "Anzahl Türen",
+    "cars_col_39_rueckspiegel_value": "Rückspiegel Art",
+    "cars_col_55_keine_dachlast_value": "Keine Dachlast",
+    "cars_col_69_reifen_felgen_kombination_value": "Reifen/Felgen Kombi (69)", 
+    "cars_col_69_reifen_felgen_value": "Reifen/Felgen (69)", 
+    "cars_col_70_reifen_felgen_kombination_value": "Reifen/Felgen Kombi (70)", 
+    "cars_col_70_reifen_felgen_value": "Reifen/Felgen (70)", 
+    "cars_col_71_reifen_felgen_kombination_value": "Reifen/Felgen Kombi (71)", 
+    "cars_col_71_reifen_felgen_value": "Reifen/Felgen (71)", 
+    "cars_bemerkungen_z1_value": "Bemerkung Z1", "cars_bemerkungen_z2_value": "Bemerkung Z2", "cars_bemerkungen_z3_value": "Bemerkung Z3", "cars_bemerkungen_z4_value": "Bemerkung Z4", "cars_bemerkungen_z5_value": "Bemerkung Z5", "cars_bemerkungen_z6_value": "Bemerkung Z6", "cars_bemerkungen_z7_value": "Bemerkung Z7", "cars_bemerkungen_z8_value": "Bemerkung Z8", "cars_bemerkungen_z9_value": "Bemerkung Z9", "cars_bemerkungen_z10_value": "Bemerkung Z10", "cars_bemerkungen_z11_value": "Bemerkung Z11", "cars_bemerkungen_z12_value": "Bemerkung Z12", "cars_bemerkungen_z13_value": "Bemerkung Z13", "cars_bemerkungen_z14_value": "Bemerkung Z14", "cars_bemerkungen_z15_value": "Bemerkung Z15", "cars_bemerkungen_z16_value": "Bemerkung Z16", "cars_bemerkungen_z17_value": "Bemerkung Z17", "cars_bemerkungen_z18_value": "Bemerkung Z18", "cars_bemerkungen_z19_value": "Bemerkung Z19", "cars_bemerkungen_z20_value": "Bemerkung Z20", "cars_bemerkungen_z21_value": "Bemerkung Z21", "cars_bemerkungen_z22_value": "Bemerkung Z22", "cars_bemerkungen_z23_value": "Bemerkung Z23", "cars_bemerkungen_z24_value": "Bemerkung Z24",
+
+    # Von/Bis pairs - base names for combined fields
+    "cars_col_40_laenge": "Länge", "cars_col_41_breite": "Breite", "cars_col_42_hoehe": "Höhe",
+    "cars_col_44_abstand_achse_1_2": "Abstand Achse 1-2", "cars_col_45_abstand_achse_2_3": "Abstand Achse 2-3", "cars_col_46_abstand_achse_3_4": "Abstand Achse 3-4",
+    "cars_col_47_spur_achse_1": "Spur Achse 1", "cars_col_48_spur_achse_2": "Spur Achse 2", "cars_col_49_spur_achse_3": "Spur Achse 3", "cars_col_50_spur_achse_4": "Spur Achse 4",
+    "cars_col_52_leergewicht": "Leergewicht", "cars_col_53_garantiegewicht": "Gesamtgewicht",
+    "cars_col_43_ueberhang_hinten": "Überhang Hinten", 
+    "cars_col_54_achsgarantie_v": "Achslast Vorne", 
+    "cars_col_54_achsgarantie_h": "Achslast Hinten", 
+    
+    # Emissions table fields
+    "emissions_abgasreinigung_value": "Abgasreinigung (Emissionen)",
+    "emissions_abgascode_value": "Abgascode", "emissions_emissionscode_value": "Emissionscode",
+    "emissions_geraeuschcode_value": "Geräuschcode", "emissions_bemerkung_value": "Bemerkung (Emissionen)",
+    "emissions_vmax": "Vmax", 
+    "emissions_hubraum": "Hubraum", "emissions_leistung": "Leistung", "emissions_leistung_bei_n_min": "Leistung bei n/min",
+    "emissions_drehmoment": "Drehmoment", "emissions_drehmoment_bei_n_min": "Drehmoment bei n/min",
+    "emissions_fahrgeraeusch": "Fahrgeräusch", "emissions_standgeraeusch": "Standgeräusch",
+    "emissions_standgeraeusch_bei_n_min": "Standgeräusch bei n/min",
+    "emissions_tc_consumption": "Verbrauch (TC)",
+    "emissions_iachse": "I-Achse (Emissionen)", 
+    # Consumption table fields
+    "consumption_treibstoff_value": "Treibstoff (Verbrauch)", "consumption_getriebe_value": "Getriebe (Verbrauch)",
+    "consumption_hinweis_value": "Hinweis (Verbrauch)",
+    "consumption_el_reichweite_wltp": "Elektrische Reichweite WLTP", 
+    "consumption_el_verbrauch_wltp": "Elektrischer Verbrauch WLTP",
+    "cars_col_55_dachlast": "Dachlast", 
+    "consumption_energieeffizienzkategorie_value": "Energieeffizienzkategorie",
+}
+
+# --- Data Grouping for Display ---
+DATA_GROUPS_ORDER = [
+    "Fahrzeug", "Sitze & Türen", "Motor", "Getriebe", "Bremsen", "Masse & Abmessungen", "Achsen & Spur", 
+    "Gewichte", "Räder & Reifen", "Leistungsdaten", "Verbrauch & Emissionen", 
+    "Bemerkungen", "Sonstige Daten" 
+]
+
+DATA_GROUPS_MAPPING = {
+    # Fahrzeug
+    "TG-Code (Typen&shy;genehmigungs&shy;nummer)": "Fahrzeug", "Fahrzeugart": "Fahrzeug", "Fahrzeugklasse": "Fahrzeug",
+    "Marke": "Fahrzeug", "Typ": "Fahrzeug", "Karosserieform": "Fahrzeug", "EU Gesamtgenehmigung": "Fahrzeug",
+    "cars_col_05_typ_variante_version": "Fahrzeug", "Hersteller (Code 10)": "Fahrzeug", "Herstellerplakette": "Fahrzeug", 
+    "Fahrgestellnummer Struktur": "Fahrzeug", "Fahrgestellnummer": "Fahrzeug",
+    "Rückspiegel Art": "Fahrzeug", "Vorziffer": "Fahrzeug", "EFKO Code": "Fahrzeug",
+
+    # Sitze & Türen
+    "Anzahl Türen": "Sitze & Türen", "Anzahl Plätze Vorne": "Sitze & Türen", "Anzahl Plätze Total": "Sitze & Türen",
+
+    # Motor
+    "Motor Marke": "Motor", "Motor Typ": "Motor", "Motor Bauart": "Motor", "Treibstoff (Motor)": "Motor",
+    "Motorkennzeichen Art": "Motor", "Motorkennzeichen": "Motor", "Motorkennzeichen Anbringungsort": "Motor",
+    "Hubraum": "Motor", "Leistung bei n/min": "Motor", 
+    "Drehmoment": "Motor", "Drehmoment bei n/min": "Motor",
+
+    # Getriebe
+    "Getriebe 1 Art": "Getriebe", "Getriebe 1 Zuordnung": "Getriebe",
+    "Getriebe 2 Art": "Getriebe", "Getriebe 2 Zuordnung": "Getriebe",
+    "Getriebe 3 Art": "Getriebe", "Getriebe 3 Zuordnung": "Getriebe",
+    "Getriebe 4 Art": "Getriebe", "Getriebe 4 Zuordnung": "Getriebe", "I-Achse (Emissionen)": "Getriebe",
+    "Achsantrieb": "Getriebe", 
+
+    # Bremsen
+    "Betriebsbremse Z1": "Bremsen", "Betriebsbremse Z2": "Bremsen", "Betriebsbremse Z3": "Bremsen",
+    "Hilfsbremse Z1": "Bremsen", "Hilfsbremse Z2": "Bremsen", "Feststellbremse": "Bremsen",
+
+    # Masse & Abmessungen
+    "Länge": "Masse & Abmessungen", "Breite": "Masse & Abmessungen", "Höhe": "Masse & Abmessungen",
+    "Überhang Hinten": "Masse & Abmessungen",
+    # Achsen & Spur
+    "Achsen/Räder": "Achsen & Spur", "Federung": "Achsen & Spur", "Lenkung": "Achsen & Spur",
+    "Abstand Achse 1-2": "Achsen & Spur", "Abstand Achse 2-3": "Achsen & Spur", "Abstand Achse 3-4": "Achsen & Spur",
+    "Spur Achse 1": "Achsen & Spur", "Spur Achse 2": "Achsen & Spur", "Spur Achse 3": "Achsen & Spur", "Spur Achse 4": "Achsen & Spur",
+
+    # Gewichte
+    "Leergewicht": "Gewichte", "Gesamtgewicht": "Gewichte", "Keine Dachlast": "Gewichte",
+    "Achslast Vorne": "Gewichte", "Achslast Hinten": "Gewichte", "Dachlast": "Gewichte",
+
+    # Räder & Reifen
+    "Reifen/Felgen Kombi (69)": "Räder & Reifen", "Reifen/Felgen (69)": "Räder & Reifen",
+    "Reifen/Felgen Kombi (70)": "Räder & Reifen", "Reifen/Felgen (70)": "Räder & Reifen",
+    "Reifen/Felgen Kombi (71)": "Räder & Reifen", "Reifen/Felgen (71)": "Räder & Reifen",
+
+    # Leistungsdaten
+    "Vmax": "Leistungsdaten", "Leistung": "Leistungsdaten",
+
+    # Verbrauch & Emissionen
+    "Abgasreinigung": "Verbrauch & Emissionen", "Abgasreinigung (Emissionen)": "Verbrauch & Emissionen",
+    "Schalldämpfer 1 Art": "Verbrauch & Emissionen", "Schalldämpfer 1 Bez.": "Verbrauch & Emissionen",
+    "Schalldämpfer 2 Art": "Verbrauch & Emissionen", "Schalldämpfer 2 Bez.": "Verbrauch & Emissionen",
+    "Schalldämpfer 3 Art": "Verbrauch & Emissionen", "Schalldämpfer 3 Bez.": "Verbrauch & Emissionen",
+    "Geräuschdämpfung Z1": "Verbrauch & Emissionen", "Geräuschdämpfung Z2": "Verbrauch & Emissionen",
+    "Geräuschdämpfung Z3": "Verbrauch & Emissionen", "Geräuschdämpfung Z4": "Verbrauch & Emissionen",
+    "Abgascode": "Verbrauch & Emissionen", "Emissionscode": "Verbrauch & Emissionen", "Geräuschcode": "Verbrauch & Emissionen",
+    "Fahrgeräusch": "Verbrauch & Emissionen", "Standgeräusch": "Verbrauch & Emissionen", "Standgeräusch bei n/min": "Verbrauch & Emissionen",
+    "Verbrauch (TC)": "Verbrauch & Emissionen", "Elektrischer Verbrauch WLTP": "Verbrauch & Emissionen",
+    "Elektrische Reichweite WLTP": "Verbrauch & Emissionen", "Energieeffizienzkategorie": "Verbrauch & Emissionen",
+    "Treibstoff (Verbrauch)": "Verbrauch & Emissionen", "Getriebe (Verbrauch)": "Verbrauch & Emissionen", "Hinweis (Verbrauch)": "Verbrauch & Emissionen",
+    "Consumption El Reichweite Von Wltp": "Verbrauch & Emissionen", 
+
+    # Bemerkungen (will be caught by fallback if labels match "Bemerkung Zx")
+}
+
+# --- Labels to Exclude from Output ---
+LABELS_TO_EXCLUDE = {
+    "Keine Anhängelast",
+    "Leistung (Fahrzeug)",
+    "Fahrzeug Vmax Mech",
+    "Emissions Leergewicht",
+    "Emissions Garantiegewicht",
+    "TG-Code (Verbrauch)",
+    "TG-Code (Emissionen)",
+    "Consumption Laenge",
+    "Consumption Breite",
+    "Consumption Hoehe",
+    "Consumption Iachse",
+    "Marke (Emissionen)",
+    "Typ (Emissionen)",
+    "Getriebe (Emissionen)",
+    "Motor Marke (Emissionen)",
+    "Motor Typ (Emissionen)",
+    "Bauart (Emissionen)",
+    "Treibstoff (Emissionen)",
+    "Achsen/Räder (Emissionen)",
+    "Antrieb (Emissionen)",
+    "Marke (Verbrauch)",
+    "Typ (Verbrauch)",
+}
+
+# --- Field Units ---
+FIELD_UNITS = {
+    "Länge": "mm",
+    "Breite": "mm",
+    "Höhe": "mm",
+    "Abstand Achse 1-2": "mm",
+    "Abstand Achse 2-3": "mm",
+    "Abstand Achse 3-4": "mm",
+    "Spur Achse 1": "mm",
+    "Spur Achse 2": "mm",
+    "Spur Achse 3": "mm",
+    "Spur Achse 4": "mm",
+    "Leergewicht": "kg",
+    "Gesamtgewicht": "kg",
+    "Vmax": "km/h",
+    "Hubraum": "cm³",
+    "Leistung": "kW",
+    "Leistung bei n/min": "rpm", 
+    "Drehmoment": "Nm",
+    "Drehmoment bei n/min": "rpm", 
+    "Fahrgeräusch": "dB(A)",
+    "Standgeräusch": "dB(A)",
+    "Standgeräusch bei n/min": "rpm", 
+    "Überhang Hinten": "mm", 
+    "Achslast Vorne": "kg", 
+    "Achslast Hinten": "kg", 
+    "Verbrauch (TC)": "l/100km", 
+    "Dachlast": "kg", 
+    "Elektrische Reichweite WLTP": "km", 
+    "Consumption El Reichweite Von Wltp": "km", 
+    "Elektrischer Verbrauch WLTP": "Wh/km", 
+}
---
+++ static/js/main.js
@@ -0,0 +1,276 @@
+// SCRIPT BLOCK 1: GLOBAL DEFINITIONS FOR HISTORY
+const MAX_HISTORY_ITEMS = 10;
+const HISTORY_COOKIE_NAME = "carSearchHistory";
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie =
+    name + "=" + (value || "") + expires + "; path=/; SameSite=Lax";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+window.addSearchToHistory = function (tgCode, displayName) {
+  if (!tgCode || !displayName) return;
+  let history = [];
+  const savedHistory = getCookie(HISTORY_COOKIE_NAME); // Uses global getCookie
+  if (savedHistory) {
+    try {
+      history = JSON.parse(savedHistory);
+    } catch (e) {
+      console.error(
+        "Error parsing search history cookie in addSearchToHistory",
+        e
+      );
+      history = [];
+    }
+  }
+  history = history.filter((item) => item.tgCode !== tgCode);
+  history.unshift({ tgCode: tgCode, display: displayName });
+  if (history.length > MAX_HISTORY_ITEMS) {
+    history = history.slice(0, MAX_HISTORY_ITEMS);
+  }
+  setCookie(HISTORY_COOKIE_NAME, JSON.stringify(history), 180); // Uses global setCookie
+};
+
+// SCRIPT BLOCK 2: DOM MANIPULATION AND EVENT LISTENERS
+function setupAutocomplete(
+  inputId,
+  suggestionsId,
+  endpointUrl,
+  isTypField = false
+) {
+  const inputField = document.getElementById(inputId);
+  const suggestionsContainer = document.getElementById(suggestionsId);
+  let debounceTimer;
+
+  inputField.addEventListener("input", function () {
+    clearTimeout(debounceTimer);
+    const term = this.value;
+
+    debounceTimer = setTimeout(async () => {
+      suggestionsContainer.innerHTML = ""; // Clear previous suggestions
+      suggestionsContainer.style.display = "none";
+
+      if (term.length < 1) {
+        // Minimum characters to trigger autocomplete
+        return;
+      }
+
+      let currentEndpointUrl = `${endpointUrl}?term=${encodeURIComponent(
+        term
+      )}`;
+      if (isTypField) {
+        const markeValue = document.getElementById("marke_input").value;
+        if (markeValue && markeValue.trim()) {
+          // Ensure markeValue is not empty and not just whitespace
+          currentEndpointUrl += `&marke=${encodeURIComponent(
+            markeValue.trim()
+          )}`;
+        }
+      }
+
+      try {
+        const response = await fetch(currentEndpointUrl);
+        if (!response.ok) {
+          console.error(
+            "Autocomplete fetch error:",
+            response.status,
+            response.statusText
+          );
+          return;
+        }
+        const suggestions = await response.json();
+
+        if (suggestions.length > 0) {
+          suggestionsContainer.style.display = "block";
+          suggestions.forEach((suggestion) => {
+            const div = document.createElement("div");
+            div.textContent = suggestion;
+            div.addEventListener("click", function () {
+              inputField.value = suggestion;
+              suggestionsContainer.innerHTML = "";
+              suggestionsContainer.style.display = "none";
+            });
+            suggestionsContainer.appendChild(div);
+          });
+        }
+      } catch (error) {
+        console.error("Autocomplete request failed:", error);
+      }
+    }, 250); // 250ms debounce delay
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !inputField.contains(event.target) &&
+      !suggestionsContainer.contains(event.target)
+    ) {
+      suggestionsContainer.innerHTML = "";
+      suggestionsContainer.style.display = "none";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupAutocomplete("marke_input", "marke_suggestions", "/autocomplete/marken");
+  setupAutocomplete(
+    "typ_input",
+    "typ_suggestions",
+    "/autocomplete/typen",
+    true
+  ); // Pass true for isTypField
+
+  function displaySearchHistory() {
+    const historyList = document.getElementById("search_history_list");
+    historyList.innerHTML = ""; // Clear previous items
+    const savedHistory = getCookie(HISTORY_COOKIE_NAME);
+    let historyItems = []; // Initialize as empty array
+
+    if (savedHistory) {
+      try {
+        historyItems = JSON.parse(savedHistory);
+      } catch (e) {
+        console.error("Error parsing search history cookie for display", e);
+        historyItems = []; // Reset to empty on error if parsing fails
+      }
+    }
+
+    if (historyItems && historyItems.length > 0) {
+      historyItems.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.display;
+        li.dataset.tgcode = item.tgCode;
+        li.addEventListener("click", function () {
+          document.getElementById("tg_code_input").value = this.dataset.tgcode;
+          document.getElementById("marke_input").value = "";
+          document.getElementById("typ_input").value = "";
+          document.querySelector(".search-form form").submit();
+        });
+        historyList.appendChild(li);
+      });
+    } else {
+      const li = document.createElement("li");
+      li.textContent = "No history yet.";
+      li.style.cursor = "default";
+      historyList.appendChild(li);
+    }
+  }
+
+  // Accordion Logic
+  const accHeaders = document.querySelectorAll(".accordion-header");
+  accHeaders.forEach((header) => {
+    header.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+      // Save open accordion state to cookie
+      const openAccordions = [];
+      accHeaders.forEach((h) => {
+        if (h.classList.contains("active")) {
+          openAccordions.push(h.textContent.trim());
+        }
+      });
+      setCookie("openAccordions", JSON.stringify(openAccordions), 7); // Uses global setCookie
+    });
+  });
+
+  // Restore accordion state from cookie or open the first one
+  if (accHeaders.length > 0) {
+    const carDetailsTitle = document.querySelector("h2");
+    if (
+      carDetailsTitle &&
+      carDetailsTitle.textContent.trim() === "Car Details"
+    ) {
+      const savedOpenAccordions = getCookie("openAccordions");
+      if (savedOpenAccordions) {
+        try {
+          const openLabels = JSON.parse(savedOpenAccordions);
+          accHeaders.forEach((header) => {
+            if (openLabels.includes(header.textContent.trim())) {
+              header.classList.add("active");
+              header.nextElementSibling.style.display = "block";
+            }
+          });
+        } catch (e) {
+          console.error("Error parsing accordion cookie", e);
+        }
+      } else {
+        const firstHeader = accHeaders[0];
+        firstHeader.classList.add("active");
+        firstHeader.nextElementSibling.style.display = "block";
+      }
+    }
+  }
+
+  const historyBtn = document.getElementById("search_history_btn");
+  const historyContainer = document.getElementById("search_history_container");
+
+  if (historyBtn && historyContainer) {
+    historyBtn.addEventListener("click", function (event) {
+      event.stopPropagation();
+      const isVisible = historyContainer.style.display === "block";
+      if (isVisible) {
+        historyContainer.style.display = "none";
+      } else {
+        displaySearchHistory();
+        historyContainer.style.display = "block";
+      }
+    });
+
+    document.addEventListener("click", function (event) {
+      if (
+        historyContainer.style.display === "block" &&
+        !historyContainer.contains(event.target) &&
+        !historyBtn.contains(event.target)
+      ) {
+        historyContainer.style.display = "none";
+      }
+    });
+  }
+
+  // Handle clicks on TG-Codes in multiple results
+  const multipleResultsList = document.querySelector(".multiple-results-list");
+  if (multipleResultsList) {
+    multipleResultsList.addEventListener("click", function (event) {
+      let targetElement = event.target;
+      while (
+        targetElement != null &&
+        !targetElement.classList.contains("clickable-tg-code")
+      ) {
+        targetElement = targetElement.parentElement;
+      }
+
+      if (
+        targetElement &&
+        targetElement.classList.contains("clickable-tg-code")
+      ) {
+        const tgCode = targetElement.dataset.tgcode;
+        if (tgCode) {
+          document.getElementById("tg_code_input").value = tgCode;
+          document.getElementById("marke_input").value = "";
+          document.getElementById("typ_input").value = "";
+          document.querySelector(".search-form form").submit();
+        }
+      }
+    });
+  }
+});
